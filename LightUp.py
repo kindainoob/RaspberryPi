3084def4d5c9387a458affd28122f89a479292db
tatsuhiro <tatsuhiro@tatsuhiBookpuro.T-Lab2>
2018-08-16 16:37:02 +0900

    python&java

---
+++ LightUp.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+# str = '・・－・・－－－・・－－・－・・'
+str = '..-..---..--.-..'
+
+#print(str)
+
+for num in range(len(str)):
+    if str[num] == '.':
+        # print('処理A')
+        pass
+    else:
+        # print('処理B')
+    pass
---
+++ morseRet.java
@@ -0,0 +1,32 @@
+class morseRet{
+  public static void main(String[] args) {
+    String str = args[0];
+    String morse = "";
+    MorseCode mc = new MorseCode();
+
+    for(int i = 0; i < str.length(); i++){
+      char c;
+      char d;
+      String s;
+      String tmp;
+      String temp = "";
+
+      c = str.charAt(i);
+      tmp = mc.convertMorse(c);
+      for(int j = 0; j <tmp.length(); j++) {
+        d = tmp.charAt(j);
+        //System.out.println();
+        if (d == '・') {
+          s = ".";
+        }
+        else {
+          s = "-";
+        }
+         temp += s;
+      }
+      morse += temp;
+    }
+
+    System.out.println(morse);
+  }
+}
